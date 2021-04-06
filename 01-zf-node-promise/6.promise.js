faa74d5cdcacba48a57d35212de8148a8b67af6c
褚鹏飞 <540788769@qq.com>
2021-04-06 19:35:36 +0800

    add: feat: promise的实现

---
+++ 01-zf-node-promise/6.promise.js
@@ -0,0 +1,25 @@
+// 1、promsie 是一个类
+// 2、promsie接收一个函数，叫做执行器。
+// 3、当前executor中给了两个函数 用来描述promise的状态，promise 中有三个状态 成功 失败 等待 默认是等待，如果调用resolve会走成功
+//    如果调用reject 会走失败态，如果调用异常会走失败态 。
+// 4、每一个promsie实例都有一个then方法
+// 5、promsie 一旦状态改变不能更改。
+
+// promise 还是基于回调的。
+let promise = new Promise((resolve, reject) => {
+  console.log("promsie")
+  resolve()
+  // reject()
+  // throw new Error();
+})
+
+promise.then(
+  (value) => {
+    console.log("success")
+  },
+  (reason) => {
+    console.log("error")
+  }
+)
+
+console.log("ok")
