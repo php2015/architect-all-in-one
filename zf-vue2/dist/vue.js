d8a26af4dae770cdbae3b74bfe99139fc6885fd8
褚鹏飞 <540788769@qq.com>
2021-05-11 08:19:29 +0800

    add: feat: 添加vue2 注释

--- zf-vue2/dist/vue.js
+++ zf-vue2/dist/vue.js
@@ -125,13 +125,23 @@
     observe(data);
   }
 
+  /**
+   * 将构造函数通过参数传递进去
+   * 表示在vue的基础上做一次混合操作 
+   * 这里参数是vue也是很好理解呀
+   * 最终都是想要在vue上做混合操作
+   * @param {*} Vue 
+   */
+
   function initMixin(Vue) {
     // 扩展原型上的方法
     Vue.prototype._init = function (options) {
       // 原型方法中的this指向实例 所有的实例 都具有这些方法
-      var vm = this; // 用户传递进来的选项挂载到上面
+      var vm = this; // 用户传递进来的选项挂载到上面,我们能够操作 vm.$options
 
-      vm.$options = options; // 初始化状态
+      vm.$options = options; // 初始化状态 为什么要有这个 函数 不仅仅是 有watch
+      // 还有computed props data 我们需要有一个统一的函数
+      // 来处理这些参数。
 
       initState(vm);
     };
--- zf-vue2/src/init.js
+++ zf-vue2/src/init.js
@@ -1,13 +1,22 @@
 import { initState } from "./state"
-// 将构造函数通过参数传递进去
+
+/**
+ * 将构造函数通过参数传递进去
+ * 表示在vue的基础上做一次混合操作 
+ * 这里参数是vue也是很好理解呀
+ * 最终都是想要在vue上做混合操作
+ * @param {*} Vue 
+ */
 export function initMixin(Vue) {
   // 扩展原型上的方法
   Vue.prototype._init = function (options) {
     // 原型方法中的this指向实例 所有的实例 都具有这些方法
     const vm = this
-    // 用户传递进来的选项挂载到上面
+    // 用户传递进来的选项挂载到上面,我们能够操作 vm.$options
     vm.$options = options
-    // 初始化状态
+    // 初始化状态 为什么要有这个 函数 不仅仅是 有watch
+    // 还有computed props data 我们需要有一个统一的函数
+    // 来处理这些参数。
     initState(vm)
   }
 }
