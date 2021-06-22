160a1a9cf662c1a54913d18bb06826bd4f729dbb
褚鹏飞 <540788769@qq.com>
2021-06-22 08:20:26 +0800

    add: feat: vue

--- zf-vue2/src/global-api/index.js
+++ zf-vue2/src/global-api/index.js
@@ -11,4 +11,40 @@ export function initGlobalApi(Vue) {
     this.options = mergeOptions(this.options, options)
     return this // 方面链式调用
   }
+
+  Vue.options._base = Vue // 无论后续创建多少个子类 都可以通过_base 找到Vue
+  Vue.options.conponents = {}
+
+
+  /**
+   * 我们在自定义全局组件的时候，会使用这个api进行声明
+   *
+   */
+  Vue.component = function (id, definition) {
+    // 为了保证父子关系，我需要产生一个新的实例，
+    // 这样做的目的是使得组件能够隔离，每个组件都会产生一个新的类
+    // 去继承父类。
+    definition = this.options._base.extend(definition)
+    // 处理过之后 definition 就是一个类 这里做一个组件的映射表
+    this.options.conponents[id] = definition
+
+    console.log(this.options.conponents);
+  }
+  /**
+   * 其实就是返回一个类
+   * extend 方法就死产生一个继承与Vue的类
+   * 并且身上有父类的功能
+   */
+  Vue.extend = function (opts) {
+    const Super = this
+    // Sub 是一个函数
+    const Sub = function VueComponent() {
+      this._init()
+    }
+    Sub.prototype = Object.create(Super.prototype)
+    Sub.prototype.constructor = Sub // 原型继承
+    // 只和 vue的options合并
+    Sub.options = mergeOptions(Super.options, opts)
+    return Sub
+  }
 }
