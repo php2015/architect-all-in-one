bd88db45a7dee0f48303a2a46387c07420e3411d
褚鹏飞 <540788769@qq.com>
2021-07-16 05:47:27 +0800

    feat: 添加webpack babel 配置

--- zf-webpack4/src/index.js
+++ zf-webpack4/src/index.js
@@ -1,1 +1,11 @@
-console.log('index.js')
+let fn = () => {
+  console.log("babel")
+}
+
+fn()
+
+class A {
+  a = 1;
+}
+let a = new A();
+console.log(a.a)
--- zf-webpack4/webpack.config.js
+++ zf-webpack4/webpack.config.js
@@ -11,12 +11,22 @@ module.exports = {
     filename: "[name][hash:6].js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // 用babel-loader 需要把es6转换成es5
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        "** /*",
-      ],
-    }),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
