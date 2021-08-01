c50b76d0596d0bd1263998b196b89b8071d33ffc
褚鹏飞 <540788769@qq.com>
2021-08-01 20:09:24 +0800

    feat: 更新vue源码

--- zf-vue2/202012vue/1.vue-init/rollup.config.js
+++ zf-vue2/202012vue/1.vue-init/rollup.config.js
@@ -1,18 +1,18 @@
-import babel from 'rollup-plugin-babel'
+import babel from "rollup-plugin-babel" // rollup 打包可以采用babel
 export default {
-    input:'./src/index.js',
-    output:{
-        format:'umd', // 支持amd 和 commonjs规范 window.Vue
-        name:'Vue',
-        file:'dist/vue.js',
-        sourcemap:true, // es5 -> es6源代码
-    },
-    plugins:[
-        babel({ // 使用babel进行转化 但是拍出node_modules 文件
-            exclude:'node_modules/**', // glob 语法
-        })
-    ]
+  input: "./src/index.js", // 打包的出口
+  output: {
+    format: "umd", // 支持amd 和 commonjs规范 window.Vue
+    name: "Vue",
+    file: "dist/vue.js",
+    sourcemap: true, // 调试功能 es5 -> es6源代码
+  },
+  plugins: [
+    babel({
+      // 使用babel进行转化 但是排除node_modules文件
+      exclude: "node_modules/**", // glob 语法 不去编译下面的文件夹
+    }),
+  ],
 }
-
 
 // 后续 需要打包不同的类型 可以写个列表，循环打包
