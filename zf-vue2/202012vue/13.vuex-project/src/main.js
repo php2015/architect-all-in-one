852def78059804dd6112ff681473e9b3a7d271f6
褚鹏飞 <540788769@qq.com>
2021-08-15 18:32:01 +0800

    feat: vuex 源码学习

--- zf-vue2/202012vue/13.vuex-project/src/main.js
+++ zf-vue2/202012vue/13.vuex-project/src/main.js
@@ -1,14 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue"
+import App from "./App.vue"
+import store from "./store"
 
 Vue.config.productionTip = false
 
 let vm = new Vue({
-  name:'root',
-   store, // 此store的目的是让所有组件都能访问到store对象
-  render: h => h(App)
-}).$mount('#app')
-
+  name: "root",
+  store, // 此store的目的是让所有组件都能访问到store 从根组件注入
+  render: (h) => h(App),
+}).$mount("#app")
 
 // 所有组件都能执行的方法 Vue.mixin({beforeCreate}), 拿到store挂载到自己的身上
